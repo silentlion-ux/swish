19f3295e9e8cd7f99e06c87dd48eaa415db4eb5d
silentlion-ux <silentlion@live.co.uk>
2026-05-09 09:44:21 +0100

    Update Swish.js

--- Swish.js
+++ Swish.js
@@ -8,11 +8,12 @@ for (let i = 0; i < items.length; i++) {
 }
 
 function expand(e) {
+  for (x in items){ items[x].style.flex = 1; }
   e.currentTarget.style.flex = 2;
   e.currentTarget.querySelector("img").style.transform = "scale(1.2)";
 }
 
 function shrink(e) {
-  e.currentTarget.style.flex = 0;
+  for (x in items){ items[x].style.flex = 0; }
   e.currentTarget.querySelector("img").style.transform = "scale(1)";
 }
