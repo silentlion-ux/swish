5d2dd4839ffd51894499363a90c346306615d0f4
silentlion-ux <silentlion@live.co.uk>
2026-05-09 09:05:53 +0100

    Add files via upload

---
+++ swish.js
@@ -0,0 +1,18 @@
+console.log("JS loaded");
+
+const items = document.getElementsByClassName("main-swiper__item");
+
+for (let i = 0; i < items.length; i++) {
+  items[i].addEventListener("mouseenter", expand);
+  items[i].addEventListener("mouseleave", shrink);
+}
+
+function expand(e) {
+  e.currentTarget.style.flex = 2;
+  e.currentTarget.querySelector("img").style.transform = "scale(1.2)";
+}
+
+function shrink(e) {
+  e.currentTarget.style.flex = 0;
+  e.currentTarget.querySelector("img").style.transform = "scale(1)";
+}
